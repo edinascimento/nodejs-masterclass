af77b25a7ed62be51d07617e411e1b1ca1131ab8
Édipo Nascimento <esnedipo@gmail.com>
2021-08-13 12:13:45 -0300

    added https support

--- index.js
+++ index.js
@@ -5,12 +5,40 @@
 
 // Libryres
 var http = require('http');
+var https = require('https');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-// The server should respond to all requests with a string
-var server = http.createServer(function(req, res){
-  
+// Instantiate the HTTP server
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+
+// start the http server
+httpServer.listen(config.httpPort, function () {
+  console.log(`Server listening on http://localhost:${config.httpPort} in ${config.envName} mode.`);
+});
+
+// Instantiate the HTTPS server
+var options = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = https.createServer(options, function (req, res) {
+  unifiedServer(req, res);
+});
+
+
+// start the https server
+httpsServer.listen(config.httpsPort, function () {
+  console.log(`Server listening on http://localhost:${config.httpsPort} in ${config.envName} mode.`);
+});
+
+
+// All the logic for both the HTTP and HTTPS server
+var unifiedServer = function (req, res) {
   // Get the URL and parse it
   var parsedUrl = new URL(req.url, "http:localhost:3000/");
   console.log(parsedUrl)
@@ -31,14 +59,14 @@ var server = http.createServer(function(req, res){
   // Get the payload, if there's any.
   var decoder = new StringDecoder('utf-8');
   var buffer = '';
-  req.on('data', function(data) {
+  req.on('data', function (data) {
     buffer += decoder.write(data);
   });
-  req.on('end', function() {
+  req.on('end', function () {
     buffer += decoder.end();
-  
+
     // Choose the handler this request should go to. If one is not found, use the notFound handler
-    var chosenHandler = typeof(router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound;
+    var chosenHandler = typeof (router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound;
 
     // Constroct the data object to send to the handler
     var data = {
@@ -46,17 +74,17 @@ var server = http.createServer(function(req, res){
       queryStringObject,
       method,
       headers,
-      'payload' : buffer
+      'payload': buffer
     }
 
     // Route the request to the handler specified in the router
-    chosenHandler(data, function(statusCode, payload) {
+    chosenHandler(data, function (statusCode, payload) {
 
       // Uset the statuc ode calleds back by the handler, or default to an empty object
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
 
       // Yse the payload called back by the handler, or default to an empty object
-      payload = typeof(payload) == 'object' ? payload : {};
+      payload = typeof (payload) == 'object' ? payload : {};
 
       var payloadString = JSON.stringify(payload);
 
@@ -70,24 +98,20 @@ var server = http.createServer(function(req, res){
       console.log('Returning this response: ', statusCode, payloadString);
     });
   });
-});
+};
 
-// start the server, and have it listen on port 3000
-server.listen(config.httpPort, function() {
-  console.log(`Server listening on http://localhost:${config.httpPort} in ${config.envName} mode.`);
-});
 
 // Define the handlers
 var handlers = {};
 
 // Sample handler
-handlers.sample = function(data, callback) {
+handlers.sample = function (data, callback) {
   // Callback a http status code, and a payload
-  callback(406, {'ping': 'pong'})
+  callback(406, { 'ping': 'pong' })
 }
 
 // NotFound handler
-handlers.notFound = function(data, callback) {
+handlers.notFound = function (data, callback) {
   callback(404);
 }
 
@@ -95,3 +119,4 @@ handlers.notFound = function(data, callback) {
 var router = {
   'sample': handlers.sample
 };
+
