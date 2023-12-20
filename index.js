250e2ea6c23b4451776fce1c669da1c276b15ac1
Edipo Nascimento <esnedipo@gmail.com>
2023-12-20 17:55:20 -0300

    Added read data functionality

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ var _data = require('./lib/data')
 
 // TESTING
 // @TODO delete this
-_data.create('test', 'newFile', {'foo':'bar'}, function(err) {
-
-    console.log('this was the error', err)
-
+_data.read('test', 'teste', (err, data) => {
+  console.log(`This was the error: ${err}, this was the data: ${data}`);
 })
 
 // Instantiate the HTTP server
